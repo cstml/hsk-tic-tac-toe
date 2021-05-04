2d69299ad3f89ad4dcdddd3ba8e53f90d5bf9eaa
cstml <cstmlcodes@gmail.com>
2021-05-04 13:25:06 +0100

    update: handling overlap a bit better       | also split the main function as it was just too big

--- TicTacToe.hs
+++ TicTacToe.hs
@@ -23,7 +23,6 @@ makeMove :: GameState -> PlayerChoice -> GameState
 makeMove g c
   | valid c   = place g c
   | otherwise = error "Not a valid choice - Position is Full"
-  -- [TODO]should make this part of the IO () so it fails safe
   where
     place ::  GameState -> PlayerChoice -> GameState
     place (g:gs) (p,  1)  = Just p : gs
@@ -42,45 +41,56 @@ printB :: Board -> IO()
 printB [] = print ""
 printB b  = print (take 3 b) >>= \_ -> printB  (drop 3 b)
 
+
+
+gameLoop :: Board -> Player -> IO()
+gameLoop st pl 
+  | won st    = printB st >> print ("Player " ++ show otP ++ " Won!")
+  | draw st   = printB st >> print ("It's a draw !")
+  | otherwise = 
+      print "Current State" >> printB st >>
+      print ("Player " ++ show pl ++ " to go!") >>
+      getLine >>= \inp ->
+      return (read inp::Int) >>= \pInp ->
+      case isValid pInp of
+        True -> gameLoop (aux pInp pl st) otP
+        _    -> print "Bad Input \n Try again! with a number from 1-9!" >> gameLoop st pl
+  where
+    aux :: Int -> Player -> Board -> Board
+    aux po pl b = makeMove b (pl,po)
+
+    isValid :: Int -> Bool
+    isValid x =  x > 0
+             &&  x < 10
+             && (st !! (x-1)) == Nothing
+    
+    otherPlayer :: XO -> XO
+    otherPlayer X = O
+    otherPlayer O = X
+        
+    otP = otherPlayer pl
+        
+    draw :: Board -> Bool
+    draw x = foldl1 (&&) (fmap (\z -> z /= Nothing) x)
+        
+    -- Probably should have made a nicer function but I like this
+    won :: Board -> Bool
+    won (a : b : c :
+         d : e : f :
+         g : h : i : [])
+      | a == b && b == c && c /= Nothing = True -- l1
+      | d == e && e == f && d /= Nothing = True -- l2
+      | g == h && h == i && i /= Nothing = True -- l3
+      | a == e && e == i && i /= Nothing = True -- d1
+      | g == e && e == c && c /= Nothing = True -- d2
+      | a == d && d == g && g /= Nothing = True -- c1
+      | b == e && e == h && h /= Nothing = True -- c2
+      | c == f && f == i && i /= Nothing = True -- c3
+      | otherwise                        = False
+        
+
+
 play :: IO()
 play =
   putStrLn "Hello and welcome to TicTacToe" >>
   gameLoop initState X
-  where 
-    gameLoop :: Board -> Player -> IO()
-    gameLoop st pl 
-      | won st    = printB st >> print ("Player " ++ show otP ++ " Won!")
-      | draw st   = printB st >> print ("It's a draw !")
-      | otherwise = 
-          print "Current State" >> printB st >>
-          print ("Player " ++ show pl ++ " to go!") >>
-          getLine >>= \inp ->
-          gameLoop (aux(read(inp)::Int) pl st) otP
-      where
-        aux :: Int -> Player -> Board -> Board
-        aux po pl b = makeMove b (pl,po)
-          
-        otherPlayer :: XO -> XO
-        otherPlayer X = O
-        otherPlayer O = X
-        
-        otP = otherPlayer pl
-        
-        draw :: Board -> Bool
-        draw x = foldl1 (&&) (fmap (\z -> z /= Nothing) x)
-        
-        -- Probably should have made a nicer function but I like this
-        won :: Board -> Bool
-        won (a : b : c :
-             d : e : f :
-             g : h : i : [])
-          | a == b && b == c && c /= Nothing = True -- l1
-          | d == e && e == f && d /= Nothing = True -- l2
-          | g == h && h == i && i /= Nothing = True -- l3
-          | a == e && e == i && i /= Nothing = True -- d1
-          | g == e && e == c && c /= Nothing = True -- d2
-          | a == d && d == g && g /= Nothing = True -- c1
-          | b == e && e == h && h /= Nothing = True -- c2
-          | c == f && f == i && i /= Nothing = True -- c3
-          | otherwise                        = False
-        
